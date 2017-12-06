b6706440386cd91f1f1b843980b96b8b5b6f6df2
admin <admin@DESKTOP-D0ETBQJ>
2017-12-06 22:17:41 +0800

    dao.wr

--- src/main/java/com/demo/dao/wr/UserLoginRepositoryImpl.java
+++ src/main/java/com/demo/dao/wr/UserLoginRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.demo.dao.wr;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -13,10 +16,21 @@ public class UserLoginRepositoryImpl implements UserDao {
 
 	@Override
 	public Object[] getUsers(String usersName) {
-		String hql = "select user_name,users_password,mobile_phone,status from users u where u.user_Name = '"+usersName+"'";
-		Query query = entityManager.createNativeQuery(hql);
-		Object[] list = (Object[]) query.getSingleResult();
-		return list;
+		String sql = "select user_name,users_password,mobile_phone,status from users u where u.user_Name = '"+usersName+"'";
+		try {
+			Query query = entityManager.createNativeQuery(sql);
+			Object[] list = (Object[]) query.getSingleResult();
+			return list;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
+	@Override
+	public Set<String> findRoles(String sql) {
+		Query query = entityManager.createNativeQuery(sql);
+		return new HashSet<>(query.getResultList());
+	}
+
+	
 }
