596a7dce4fbeada9d6ad5d5b038f9f8502173391
thinkpad <thinkpad@DESKTOP-3B1HDOA>
2017-12-07 17:21:14 +0800

    bb

--- src/main/java/com/demo/controller/jian/SubjectOrderRecordHandler.java
+++ src/main/java/com/demo/controller/jian/SubjectOrderRecordHandler.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.demo.model.MemberAccount;
+import com.demo.model.MemberWithdrawRecord;
 import com.demo.model.SubjectOrderRecord;
+import com.demo.service.jian.MemberAccountService;
+import com.demo.service.jian.MemberWithdrawRecordService;
 import com.demo.service.jian.SubjectOrderRecordService;
 
 /**
@@ -15,14 +19,35 @@ import com.demo.service.jian.SubjectOrderRecordService;
  * @time 2017年12月6日下午12:08:16
  */
 @Controller
+@RequestMapping("jian")
 public class SubjectOrderRecordHandler {
 	@Autowired
 	SubjectOrderRecordService subjectOrderRecordService;
-	@RequestMapping("rechargeManage")
+	@Autowired
+	MemberWithdrawRecordService  memberWithdrawRecordService;
+	@Autowired
+	MemberAccountService memberAccountService;
+	//充值管理
+	@RequestMapping("sysmember/rechargeManage")
 	public String toRechargeManage(Map<String, Object> map){
 		List<SubjectOrderRecord> olist=subjectOrderRecordService.findSubjectOrderRecordService();
 		map.put("olist", olist);
-		return "sysmember/czgl.jsp";
-		}
+		return "/sysmember/czgl";
 	}
+	//提现管理
+	@RequestMapping("sysmember/WithdrawManage")
+	public String toWithdrawManage(Map<String, Object> map){
+		List<MemberWithdrawRecord> mlist=memberWithdrawRecordService.findMemberWithdrawRecord();
+		map.put("mlist", mlist);
+		return "/sysmember/txgl";
+	}
+	//邀请奖励
+	@RequestMapping("sysmember/inviteRewards")
+	public String toinviteRewards(Map<String, Object> map){
+        List<MemberAccount> mlist=memberAccountService.findMemberAccount();
+        map.put("mlist", mlist);
+		return "/sysmember/yqjl";
+	}
+
+}
 
