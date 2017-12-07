44c4256d8a86b8fd0c5b3833884a22e60fd66491
ldd <ldd@LAPTOP-6B5D00DR>
2017-12-07 14:36:47 +0800

    ldd dao

---
+++ src/main/java/com/demo/dao/ldd/MemberaccountDao.java
@@ -0,0 +1,13 @@
+package com.demo.dao.ldd;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.model.MemberAccount;
+/**
+ * 资金查询记录
+ * @author ldd
+ *
+ */
+public interface MemberaccountDao extends JpaRepository<MemberAccount, Integer> {
+
+}
---
+++ src/main/java/com/demo/dao/ldd/MembersDao.java
@@ -0,0 +1,13 @@
+package com.demo.dao.ldd;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.model.Members;
+/**
+ * ÕËºÅ¼ÇÂ¼
+ * @author ldd
+ *
+ */
+public interface MembersDao extends JpaRepository<Members, Integer> {
+
+}
