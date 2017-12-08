51ae15065bc3e2db00cd4bbfa505670e43e57aa6
admin <admin@DESKTOP-HM5HR78>
2017-12-08 17:24:23 +0800

    userdao

--- src/main/java/com/demo/dao/wr/UserDao.java
+++ src/main/java/com/demo/dao/wr/UserDao.java
@@ -1,9 +1,24 @@
 package com.demo.dao.wr;
 
+import java.util.Set;
+
 import com.demo.model.Users;
 
 public interface UserDao {
 	
+	/**
+	 * 查询用户信息登陆
+	 * @param sql
+	 * @return
+	 */
+	
 	public Object[] getUsers(String usersName);
+	
+	/**
+	 * 查询角色
+	 * @param sql
+	 * @return
+	 */
+	public Set<String> findRoles(String sql);
 
 }
