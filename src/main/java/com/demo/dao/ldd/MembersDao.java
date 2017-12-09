2a330574cad56cb1b437c9390945bcab8a90842c
ldd <ldd@LAPTOP-6B5D00DR>
2017-12-09 08:33:28 +0800

    ldd dao

--- src/main/java/com/demo/dao/ldd/MembersDao.java
+++ src/main/java/com/demo/dao/ldd/MembersDao.java
@@ -1,7 +1,16 @@
 package com.demo.dao.ldd;
 
 import java.util.List;
+import java.util.Map;
 
 public interface MembersDao {
-	public List<Object[]> selectfinancialplanner();
+	//两表联查，理财师查询
+	public List<Object[]> selectfinancialplanner(Map<String, Object> maps,Integer page,Integer rowsize);
+	//理财师查询总数
+	public Integer getcounts(Map<String, Object> maps);
+
+	//两表联查，绑卡管理
+	public List<Object[]> selectmemberbankcards(Map<String, Object> maps,Integer page,Integer rowsize);
+	//理财师查询总数
+	public Integer getcountsbankcards(Map<String, Object> maps);
 }
--- src/main/java/com/demo/dao/ldd/MembersRepositoryImpl.java
+++ src/main/java/com/demo/dao/ldd/MembersRepositoryImpl.java
@@ -1,21 +1,110 @@
 package com.demo.dao.ldd;
 
 import java.util.List;
-
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+
 public class MembersRepositoryImpl implements MembersDao {
+
 	@PersistenceContext
 	EntityManager em;
+
+
+	//两表联查，理财师查询
 	@Override
-	public List<Object[]> selectfinancialplanner() {
-		String sql="SELECT fp.*,m.mobile_Phone FROM financial_planner fp,Members m WHERE fp.member_id =m.member_id order by fp.create_date desc";
+	public List<Object[]> selectfinancialplanner(Map<String, Object> maps,Integer page,Integer rowsize) {
+		String sql="SELECT fp.*,m.mobile_Phone FROM financial_planner fp,Members m WHERE fp.member_id =m.member_id ";
+		if (maps.get("iphone")!=null&&!"".equals(maps.get("iphone"))) {
+			sql+=" and m.mobile_Phone like'%"+maps.get("iphone")+"%'";
+		}
+		if (maps.get("name")!=null&&!"".equals(maps.get("name"))) {
+			sql+=" and fp.financial_Planner_Name like'%"+maps.get("name")+"%'";
+		}
+		if (maps.get("status")!=null&&!"".equals(maps.get("status"))) {
+			sql+=" and fp.status ="+maps.get("status");
+		}
+		if (maps.get("time")!=null&&!"".equals(maps.get("time"))) {
+			sql+=" and fp.createDate ="+maps.get("time");
+		}
+		sql+=" order by fp.create_date desc";
 		Query query=em.createNativeQuery(sql);
+		query.setFirstResult((page-1)*rowsize);
+		query.setMaxResults(rowsize);
 		List<Object[]> list=(List<Object[]>) query.getResultList();
 		return list;
 	}
+	//理财师查询总数
+	@Override
+	public Integer getcounts(Map<String, Object> maps) {
+		String sql="SELECT count(*) FROM financial_planner fp,Members m WHERE fp.member_id =m.member_id ";
+		if (maps.get("iphone")!=null&&!"".equals(maps.get("iphone"))) {
+			sql+=" and m.mobile_Phone like'%"+maps.get("iphone")+"%'";
+		}
+		if (maps.get("name")!=null&&!"".equals(maps.get("name"))) {
+			sql+=" and fp.financialPlannerName like'%"+maps.get("name")+"%'";
+		}
+		if (maps.get("status")!=null&&!"".equals(maps.get("status"))) {
+			sql+=" and fp.status ="+maps.get("status");
+		}
+		if (maps.get("time")!=null&&!"".equals(maps.get("time"))) {
+			sql+=" and fp.createDate ="+maps.get("time");
+		}
+		sql+=" order by fp.create_date desc";
+		Query query=em.createNativeQuery(sql);		
+		Object object=query.getSingleResult();
+		Integer count=Integer.parseInt(object.toString());
+		return count;
+	}
+
+	//两表联查，绑卡管理
+	@Override
+	public List<Object[]> selectmemberbankcards(Map<String, Object> maps,Integer page,Integer rowsize) {
+		String sql=" SELECT b.*,m.names,m.mobile_Phone,m.member_name,m.member_identity FROM member_bankcards b,members m WHERE b.member_id=m.member_id";
+		if (maps.get("iphone")!=null&&!"".equals(maps.get("iphone"))) {
+			sql+=" and m.mobile_Phone like'%"+maps.get("iphone")+"%'";
+		}
+		if (maps.get("name")!=null&&!"".equals(maps.get("name"))) {
+			sql+=" and fp.financial_Planner_Name like'%"+maps.get("name")+"%'";
+		}
+		if (maps.get("status")!=null&&!"".equals(maps.get("status"))) {
+			sql+=" and fp.status ="+maps.get("status");
+		}
+		if (maps.get("time")!=null&&!"".equals(maps.get("time"))) {
+			sql+=" and fp.createDate ="+maps.get("time");
+		}
+		sql+=" order by b.create_date desc";
+		Query query=em.createNativeQuery(sql);
+		query.setFirstResult((page-1)*rowsize);
+		query.setMaxResults(rowsize);
+		List<Object[]> list=(List<Object[]>) query.getResultList();
+		return list;
+	}
+	//绑卡管理查询总数
+	@Override
+	public Integer getcountsbankcards(Map<String, Object> maps) {
+		String sql="SELECT count(*) FROM member_bankcards b,members m WHERE b.member_id=m.member_id ";
+		if (maps.get("iphone")!=null&&!"".equals(maps.get("iphone"))) {
+			sql+=" and m.mobile_Phone like'%"+maps.get("iphone")+"%'";
+		}
+		if (maps.get("name")!=null&&!"".equals(maps.get("name"))) {
+			sql+=" and fp.financialPlannerName like'%"+maps.get("name")+"%'";
+		}
+		if (maps.get("status")!=null&&!"".equals(maps.get("status"))) {
+			sql+=" and fp.status ="+maps.get("status");
+		}
+		if (maps.get("time")!=null&&!"".equals(maps.get("time"))) {
+			sql+=" and fp.createDate ="+maps.get("time");
+		}
+		sql+=" order by b.create_date desc";
+		Query query=em.createNativeQuery(sql);		
+		Object object=query.getSingleResult();
+		Integer count=Integer.parseInt(object.toString());
+		return count;
+	}
+
 
 }
