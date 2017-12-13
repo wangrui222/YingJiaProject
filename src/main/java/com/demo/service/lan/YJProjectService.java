478f37bf28b6c2147e316e423acc8c911f8f404b
ldd <ldd@LAPTOP-6B5D00DR>
2017-12-13 11:30:26 +0800

    ldd service 12/13

--- src/main/java/com/demo/service/lan/YJProjectService.java
+++ src/main/java/com/demo/service/lan/YJProjectService.java
@@ -7,11 +7,13 @@ import org.springframework.data.domain.Page;
 
 import com.demo.model.FinancialPlanner;
 import com.demo.model.MemberAccount;
+import com.demo.model.MemberDepositRecord;
 import com.demo.model.MemberPucChargeItem;
 import com.demo.model.MemberTally;
 import com.demo.model.MemberTradeRecord;
 import com.demo.model.MemberWithdrawRecord;
 import com.demo.model.Members;
+import com.demo.model.SubjectPurchaseRecord;
 
 public interface YJProjectService {
 	//后台-会员管理-全部用户查询
@@ -22,11 +24,17 @@ public interface YJProjectService {
 	public MemberAccount selectmemberoneaccount(Integer mid);
 	//后台-会员管理-一个用户理财师查询
 	public FinancialPlanner selectonefinancialplanner(Integer mid);
-	//后台-会员管理-一个用户提现查询
-	public MemberWithdrawRecord selectoneonememberwithdrawrecord(Integer mid);
-	//后台-会员管理-一个用户钱包查询
-	public MemberTradeRecord selectoneonemembertraderecord(Integer mid);
 
+
+	//后台-会员管理-一个用户提现查询
+	public List<MemberWithdrawRecord> selectoneonememberwithdrawrecord(Integer mid);
+	//后台-会员管理-一个用户所有钱包查询
+	public List<MemberTradeRecord> selectoneonemembertraderecord(Integer mid);
+	//后台-会员管理-一个用户所有充值记录查询
+	public List<MemberDepositRecord> selectonememberdepositrecord(Integer mid);
+	/*//后台-会员管理-一个用户所有投资记录查询
+	public List<SubjectPurchaseRecord> selectonesubjectpurchaserecord(Integer mid);
+*/
 
 
 
@@ -34,15 +42,29 @@ public interface YJProjectService {
 	public List<Object[]> selectfinancialplanner(Map<String, Object> maps,Integer page,Integer rowsize);
 	//后台-会员管理-理财师查询总数
 	public Integer getfinacialplannercount(Map<String, Object> maps);
+	//后台-会员管理-理财师审核-修改状态
+	public void updatestatus(Integer status,Integer id);
+	
+	
 
-	//后台-会员管理-理财师审核
+	//后台-会员管理-绑卡管理
 	public List<Object[]> selectmemberbankcards(Map<String, Object> maps,Integer page,Integer rowsize);
-	//后台-会员管理-理财师查询总数
+	//后台-会员管理-绑卡管理查询总数
 	public Integer getselectmemberbankcardsrcount(Map<String, Object> maps);
-
+	//后台-会员管理-绑卡管理--修改状态
+	public void updatedelflag(Integer delflag,Integer id);
+	
+	
 	//后台-会员管理-付息计划
 	public List<Object[]> selectmembersubject(Map<String, Object> maps,Integer page,Integer rowsize);
 	//后台-会员管理-理财师查询总数
 	public Integer getselectmembersubjectcount(Map<String, Object> maps);
-
+	//后台-会员管理-付息计划-体验金付息
+	public List<Object[]> selectmembersubjectBbinpurchaserecord(Integer id,Integer page,Integer rowsize);
+	//后台-会员管理-体验金付息查询总数
+	public Integer getselectmemberBbinpurchaserecordcount(Integer id);
+	//后台-会员管理-付息计划-体验金付息
+	public List<Object[]> selectmembersubjectpurchaserecord(Integer id,Integer page,Integer rowsize);
+	//后台-会员管理-体验金付息查询总数
+	public Integer getselectmemberpurchaserecordcount(Integer id);
 }
--- src/main/java/com/demo/service/lan/YJProjectServiceImpl.java
+++ src/main/java/com/demo/service/lan/YJProjectServiceImpl.java
@@ -10,6 +10,7 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,17 +19,23 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.demo.dao.ldd.FinancialplannerRepository;
+
 import com.demo.dao.ldd.MembertraderecordRepository;
 import com.demo.dao.ldd.MemberaccountRepository;
+import com.demo.dao.ldd.MemberbankcardsRepository;
+import com.demo.dao.ldd.MemberdepositrecordRepository;
 import com.demo.dao.ldd.MembersRepository;
 import com.demo.dao.ldd.MemberwithdrawrecordRepository;
+import com.demo.dao.ldd.SubjectpurchaserecordRepository;
 import com.demo.model.FinancialPlanner;
 import com.demo.model.MemberAccount;
+import com.demo.model.MemberDepositRecord;
 import com.demo.model.MemberPucChargeItem;
 import com.demo.model.MemberTally;
 import com.demo.model.MemberTradeRecord;
 import com.demo.model.MemberWithdrawRecord;
 import com.demo.model.Members;
+import com.demo.model.SubjectPurchaseRecord;
 
 @Service
 public class YJProjectServiceImpl implements YJProjectService{
@@ -37,12 +44,17 @@ public class YJProjectServiceImpl implements YJProjectService{
 	@Autowired
 	MemberaccountRepository memberaccountrepository;
 	@Autowired
+	MemberdepositrecordRepository memberdepositrecordrepository;
+	@Autowired
 	FinancialplannerRepository financialplannerrepository;
 	@Autowired
 	MemberwithdrawrecordRepository memeberwithdrawrecordrepository;
 	@Autowired
 	MembertraderecordRepository membertraderecordrepository;
-
+	@Autowired
+	SubjectpurchaserecordRepository subjectpurchaserecordrepository;
+	@Autowired
+	MemberbankcardsRepository memberbankcarsrepository;
 
 	//后台-会员管理-全部用户查询
 	@Override
@@ -90,25 +102,38 @@ public class YJProjectServiceImpl implements YJProjectService{
 	@Override
 	public MemberAccount selectmemberoneaccount(Integer mid) {
 
-		return memberaccountrepository.findOne(mid);
+		return memberaccountrepository.selectMemberAccount(mid);
 	}
+	
 	//后台-会员管理-一个用户理财师查询
 	@Override
 	public FinancialPlanner selectonefinancialplanner(Integer mid) {
-		return financialplannerrepository.findOne(mid);
+		return financialplannerrepository.selectFinancialPlannert(mid);
 	}
 	//后台-会员管理-一个用户提现查询
 	@Override
-	public MemberWithdrawRecord selectoneonememberwithdrawrecord(Integer mid) {
-		return memeberwithdrawrecordrepository.findOne(mid);
+	public List<MemberWithdrawRecord> selectoneonememberwithdrawrecord(Integer mid) {
+		return memeberwithdrawrecordrepository.selectMemberWithdrawRecord(mid);
 	}
 	//后台-会员管理-一个用户钱包查询
 	@Override
-	public MemberTradeRecord selectoneonemembertraderecord(Integer mid) {		
-		return membertraderecordrepository.findOne(mid);
+	public List<MemberTradeRecord> selectoneonemembertraderecord(Integer mid) {		
+		return membertraderecordrepository.selectMemberTradeRecord(mid);
 	}
+	//后台-会员管理-一个用户充值记录查询
+	@Override
+	public List<MemberDepositRecord> selectonememberdepositrecord(Integer mid) {
+		return memberdepositrecordrepository.selectMemberDepositRecord(mid);
+	}
+	/*//后台-会员管理-一个用户投资记录查询
+	@Override
+	public List<SubjectPurchaseRecord> selectonesubjectpurchaserecord(Integer mid) {
+		return subjectpurchaserecordrepository.selectSubjectPurchaseRecord(mid);
+	}*/
 	
 	
+	
+
 	//后台-会员管理-理财师审核
 	@Override
 	public List<Object[]> selectfinancialplanner(Map<String, Object> maps,Integer page,Integer rowsize) {		
@@ -117,9 +142,18 @@ public class YJProjectServiceImpl implements YJProjectService{
 	//后台-会员管理-理财师查询总数
 	@Override
 	public Integer getfinacialplannercount(Map<String, Object> maps) {
-		 
+
 		return memberrepository.getcounts(maps);
 	}
+	//后台-会员管理-理财师审核-修改状态
+	@Override
+	@Transactional
+	public void updatestatus(Integer status,Integer id) {
+		financialplannerrepository.updateFinancialPlannert(status, id);		
+	}
+	
+	
+	
 	//后台-会员管理-绑卡管理
 	@Override
 	public List<Object[]> selectmemberbankcards(Map<String, Object> maps, Integer page, Integer rowsize) {	
@@ -128,6 +162,12 @@ public class YJProjectServiceImpl implements YJProjectService{
 	//后台-会员管理-绑卡查询总数
 	public Integer getselectmemberbankcardsrcount(Map<String, Object> maps) {
 		return memberrepository.getcountsbankcards(maps);
+	}
+	//后台-会员管理-绑卡管理-修改状态
+	@Override
+	@Transactional
+	public void updatedelflag(Integer delflag, Integer id) {
+		memberbankcarsrepository.updateMemberbankcards(delflag, id);
 	}
 	//后台-会员管理-付息计划
 	@Override
@@ -141,6 +181,34 @@ public class YJProjectServiceImpl implements YJProjectService{
 		return memberrepository.getcountssubject(maps);
 	}
 
+
+	//后台-会员管理-付息计划-体验金付息
+	@Override
+	public List<Object[]> selectmembersubjectBbinpurchaserecord(Integer id, Integer page, Integer rowsize) {
+		return memberrepository.selectsubjectBbinpurchaserecord(id, page, rowsize);
+	}
+	//后台-会员管理-付息计划查询总数
+	@Override
+	public Integer getselectmemberBbinpurchaserecordcount(Integer id) {
+		// TODO Auto-generated method stub
+		return memberrepository.getcountssubjectBbinpurchaserecord(id);
+	}
+	//后台-会员管理-付息计划-付息列表
+	@Override
+	public List<Object[]> selectmembersubjectpurchaserecord(Integer id, Integer page, Integer rowsize) {
+		// TODO Auto-generated method stub
+		return memberrepository.selectsubjectpurchaserecord(id, page, rowsize);
+	}
+	//后台-会员管理-付息列表查询总数
+	@Override
+	public Integer getselectmemberpurchaserecordcount(Integer id) {
+		// TODO Auto-generated method stub
+		return memberrepository.getcountssubjectpurchaserecord(id);
+	}
+	
+	
+	
+	
 
 
 }
