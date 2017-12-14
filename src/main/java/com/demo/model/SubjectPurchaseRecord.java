93fbbe59af867387edb2ede3c06640fa9b411753
ldd <ldd@LAPTOP-6B5D00DR>
2017-12-14 09:08:09 +0800

    11

--- src/main/java/com/demo/model/SubjectPurchaseRecord.java
+++ src/main/java/com/demo/model/SubjectPurchaseRecord.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
-@Entity(name="subject_purchase_record")
+@Entity
 
 /**
  * ±êµÄ¹ºÂò
