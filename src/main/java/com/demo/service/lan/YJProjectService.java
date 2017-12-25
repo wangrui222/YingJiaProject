f9ac5b353de64ffc43cc021b580fc67891734d85
ldd <ldd@LAPTOP-6B5D00DR>
2017-12-25 17:50:28 +0800

    ldd service

--- src/main/java/com/demo/service/lan/YJProjectService.java
+++ src/main/java/com/demo/service/lan/YJProjectService.java
@@ -1,5 +1,6 @@
 package com.demo.service.lan;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +15,8 @@ import com.demo.model.MemberTradeRecord;
 import com.demo.model.MemberWithdrawRecord;
 import com.demo.model.Members;
 import com.demo.model.SubjectPurchaseRecord;
+import com.demo.model.UserRole;
+import com.demo.model.Users;
 
 public interface YJProjectService {
 	//后台-会员管理-全部用户查询
@@ -34,7 +37,7 @@ public interface YJProjectService {
 	public List<MemberDepositRecord> selectonememberdepositrecord(Integer mid);
 	/*//后台-会员管理-一个用户所有投资记录查询
 	public List<SubjectPurchaseRecord> selectonesubjectpurchaserecord(Integer mid);
-*/
+	 */
 
 
 
@@ -44,8 +47,8 @@ public interface YJProjectService {
 	public Integer getfinacialplannercount(Map<String, Object> maps);
 	//后台-会员管理-理财师审核-修改状态
 	public void updatestatus(Integer status,Integer id);
-	
-	
+
+
 
 	//后台-会员管理-绑卡管理
 	public List<Object[]> selectmemberbankcards(Map<String, Object> maps,Integer page,Integer rowsize);
@@ -53,8 +56,8 @@ public interface YJProjectService {
 	public Integer getselectmemberbankcardsrcount(Map<String, Object> maps);
 	//后台-会员管理-绑卡管理--修改状态
 	public void updatedelflag(Integer delflag,Integer id);
-	
-	
+
+
 	//后台-会员管理-付息计划
 	public List<Object[]> selectmembersubject(Map<String, Object> maps,Integer page,Integer rowsize);
 	//后台-会员管理-理财师查询总数
@@ -67,4 +70,45 @@ public interface YJProjectService {
 	public List<Object[]> selectmembersubjectpurchaserecord(Integer id,Integer page,Integer rowsize);
 	//后台-会员管理-体验金付息查询总数
 	public Integer getselectmemberpurchaserecordcount(Integer id);
+
+	//==========================================================
+
+	//后台-系统设置-账户设置-全部用户查询
+	public List<Object[]> selectuses(Integer page,Integer rowsize);
+	//后台-系统设置-账户设置-全部用户查询总数
+	public Integer getusercount();
+	//后台-系统设置-账户设置-全部角色查询
+	public List<UserRole> selectuserrole();
+	//后台-系统设置-账户设置-添加账号
+	public void adduser(Users user);
+	//后台-系统设置-账户设置-添加账号时判断用户名
+	public List<Object[]> validateUserName(String uname);
+	//后台-系统设置-账户设置-按id查询一个账号
+	public Object[] selectoneuserrole(Integer uid);
+	public Users findone(Integer uid);
+	public Object findpassword(String name);
+	public Object finduserid(String name);
+	
+
+	//后台-系统设置-账户设置-修改账号
+	public void updateuser(String pwd,String phone,Date update,Integer uid);
+	public void updateuserrole(Integer roleid,Integer uid);
+	//后台-系统设置-账户设置-删除账号
+	public void deleteuser(Integer uid);
+
+
+	//后台-系统设置-角色设置-添加角色
+	public void adduserrole(UserRole userrole);
+	//后台-系统设置-密码设置-修改密码
+	public void updateuserpwd(String pwd,Integer uid);
+	//后台-系统设置-账户设置-修改账号时判断密码是否纯在
+	public List<Object[]> validateUserpwd(String pwd);
+
+
+
+
+
+
+
+
 }
