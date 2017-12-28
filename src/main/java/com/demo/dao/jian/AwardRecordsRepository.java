4474ca086c86ce7625859358b23376c9d24b82dd
zhangjian <zhangjian@DESKTOP-U84DNAI>
2017-12-28 23:02:20 +0800

    z

---
+++ src/main/java/com/demo/dao/jian/AwardRecordsRepository.java
@@ -0,0 +1,9 @@
+package com.demo.dao.jian;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.model.AwardRecords;
+
+public interface AwardRecordsRepository extends JpaRepository<AwardRecords, Integer>{
+
+}
---
+++ src/main/java/com/demo/dao/jian/MemberProfitRecordRepository.java
@@ -0,0 +1,10 @@
+package com.demo.dao.jian;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.demo.model.MemberProfitRecord;
+
+
+public interface MemberProfitRecordRepository extends JpaRepository<MemberProfitRecord, Integer>{
+
+}
